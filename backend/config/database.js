96298696f9c78fcc5513742e7c8d5a4e343788f0
Japa <pedrosilva.9195@gmail.com>
2026-03-27 21:33:07 -0300

    first commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  host: process.env.DB_HOST || 'dpg-d73h9pkg9agc738666tg-a',
+  user: process.env.DB_USER || 'impulso_web_user',
+  password: process.env.DB_PASSWORD || '4Eb9xSyX7Koxx6gRDHjIxUvGKKkfvuMk',
   database: process.env.DB_NAME || 'impulso_web',
+  port: process.env.DB_PORT || 3306, // importante!
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
